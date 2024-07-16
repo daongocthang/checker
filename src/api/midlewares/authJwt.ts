39279bf09c3741a777a47ae9973368be6e75e9db
wayfar3 <daongocthang.vn@gmail.com>
2024-07-16 13:14:43 +0700

    u

--- src/api/midlewares/authJwt.ts
+++ src/api/midlewares/authJwt.ts
@@ -1,5 +1,13 @@
 import { NextFunction, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+import { HttpStatus } from '../../constants';
 
 const verifyToken = (req: Request, res: Response, next: NextFunction) => {
-    const token = req.session['token'];
+    const sessionData = req.session as CookieSessionInterfaces.CookieSessionObject;
+    const token = sessionData.token;
+    if (!token) {
+        res.status(HttpStatus.FORBIDDEN).send({ message: 'No token provided' });
+    }
+
+    jwt.verify(token, 'secret-key', (decode: Object) => {});
 };
