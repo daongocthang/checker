35cc568dcfb525ed62e10720a79cc471d962f118
wayfar3 <daongocthang.vn@gmail.com>
2024-08-07 08:34:06 +0700

    up

--- config.js
+++
@@ -1,7 +0,0 @@
-const join = require('path').join;
-
-export default {
-    name: 'Warranty Checker',
-    description: 'Run TypeScript on Windows Service.',
-    script: join(__dirname, 'dist/index.js'),
-};
